59e9c9fcd17a478d7b790d98b7b35a1d3dd331d6
akshays-github-coder <akshaysarambale8@gmail.com>
2026-05-15 07:31:42 +0530

    Resilience4j

---
+++ resilience4j/serviceB/serviceB/src/test/java/com/codewithakshay/serviceB/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithakshay.serviceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
